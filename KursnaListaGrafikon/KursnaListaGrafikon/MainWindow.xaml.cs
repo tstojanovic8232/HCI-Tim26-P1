bca1a73bc77e6e1b1e018da25c9430e6c4d5bdfe
Teodora Stojanovic <t.stojanovic8232@gmail.com>
2022-04-18 22:40:13 +0200

    Ubačen candlestick chart

--- KursnaListaGrafikon/KursnaListaGrafikon/MainWindow.xaml.cs
+++ KursnaListaGrafikon/KursnaListaGrafikon/MainWindow.xaml.cs
@@ -12,8 +12,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
 using LiveCharts;
+using LiveCharts.Defaults;
 using LiveCharts.Wpf;
+
 namespace KursnaListaGrafikon
 {
     /// <summary>
@@ -25,37 +28,33 @@ namespace KursnaListaGrafikon
         public SeriesCollection SeriesCollection { get; set; }
         public string[] Labels { get; set; }
         public Func<double, string> YFormatter { get; set; }
-        Random rand = new Random();
-              
+
+        public SeriesCollection SeriesCollection2 { get; set; }
+        public string[] Labels2 { get; set; }
+
+
 
         public MainWindow()
         {
             
             InitializeComponent();
+
             SeriesCollection = new SeriesCollection
             {
-
                 new LineSeries
                 {
-
-                Title = "Series 1",
-
-                    Values = new ChartValues<double> {
-                        rand.Next(1, 20)
-
-                    }
+                    Title = "Series 1",
+                    Values = new ChartValues<double> { 4, 6, 5, 2 ,7 }
                 },
                 new LineSeries
                 {
                     Title = "Series 2",
-                   
-                    Values = new ChartValues<double> { rand.Next(1, 20) }
-                    
+                    Values = new ChartValues<double> { 6, 7, 3, 4 ,6 }
                 }
-                
             };
 
-            Labels = new[] { "Jan", "Feb", "Mar", "Apr", "May" };
+
+        Labels = new[] { "Jan", "Feb", "Mar", "Apr", "May" };
             YFormatter = value => value.ToString("C");
 
             //modifying the series collection will animate and update the chart
@@ -68,6 +67,58 @@ namespace KursnaListaGrafikon
             //modifying any series values will also animate and update the chart
             SeriesCollection[2].Values.Add(5d);
 
+            SeriesCollection2 = new SeriesCollection
+            {
+                new CandleSeries
+                {
+                    Values = new ChartValues<OhlcPoint>
+                    {
+                        new OhlcPoint(32, 35, 30, 32),
+                        new OhlcPoint(33, 38, 31, 37),
+                        new OhlcPoint(35, 42, 30, 40),
+                        new OhlcPoint(37, 40, 35, 38),
+                        new OhlcPoint(35, 38, 32, 33),
+                        new OhlcPoint(32, 35, 30, 32),
+                        new OhlcPoint(33, 38, 31, 37),
+                        new OhlcPoint(35, 42, 30, 40),
+                        new OhlcPoint(37, 40, 35, 38),
+                        new OhlcPoint(35, 38, 32, 33),
+                        new OhlcPoint(32, 35, 30, 32),
+                        new OhlcPoint(33, 38, 31, 37),
+                        new OhlcPoint(35, 42, 30, 40),
+                        new OhlcPoint(37, 40, 35, 38),
+                        new OhlcPoint(35, 38, 32, 33),
+                        new OhlcPoint(32, 35, 30, 32),
+                        new OhlcPoint(33, 38, 31, 37),
+                        new OhlcPoint(35, 42, 30, 40),
+                        new OhlcPoint(37, 40, 35, 38),
+                        new OhlcPoint(35, 38, 32, 33),
+                        new OhlcPoint(32, 35, 30, 32),
+                        new OhlcPoint(33, 38, 31, 37),
+                        new OhlcPoint(35, 42, 30, 40),
+                        new OhlcPoint(37, 40, 35, 38),
+                        new OhlcPoint(35, 38, 32, 33)
+                    }
+                }
+            };
+
+            //based on https://github.com/beto-rodriguez/Live-Charts/issues/166 
+            //The Ohcl point X property is zero based indexed.
+            //this means the first point is 0, second 1, third 2.... and so on
+            //then you can use the Axis.Labels properties to map the chart X with a label in the array.
+            //for more info see (mapped labels section) 
+            //http://lvcharts.net/#/examples/v1/labels-wpf?path=WPF-Components-Labels
+
+            Labels2 = new[]
+            {
+                DateTime.Now.ToString("dd MMM"),
+                DateTime.Now.AddDays(1).ToString("dd MMM"),
+                DateTime.Now.AddDays(2).ToString("dd MMM"),
+                DateTime.Now.AddDays(3).ToString("dd MMM"),
+                DateTime.Now.AddDays(4).ToString("dd MMM"),
+            };
+
+
             DataContext = this;
         }
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -75,7 +126,7 @@ namespace KursnaListaGrafikon
             var s = new TableWindow();
             s.ShowDialog();
         }
-        
+
     }
     //API key: M4HJM8TSKJCPJ1WA
 }
