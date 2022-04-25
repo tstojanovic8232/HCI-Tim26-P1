0db9d06e9c93e280af0085520e6637f78737656a
Teodora Stojanovic <t.stojanovic8232@gmail.com>
2022-04-25 13:03:46 +0200

    Updated xaml fajlovi

--- KursnaListaGrafikon/KursnaListaGrafikon/MainWindow.xaml.cs
+++ KursnaListaGrafikon/KursnaListaGrafikon/MainWindow.xaml.cs
@@ -31,14 +31,9 @@ namespace KursnaListaGrafikon
         const string apiKey = "M4HJM8TSKJCPJ1WA";
 
         LinijskiGrafikon linijski { get; set; }
-        public List<string> valute; //Za šta su nam ove valute? Da li možemo da ih obrišemo?
+        public List<string> valute;
 
-        // Dodati atributi u MainWindow jer ih koriste oba grafikona, a i lakše ih je menjati kroz EventListener za elemente
-        public string Vreme { get; set; }
-        public List<string> PocetneValute { get; set; }
-        public string KrajnjaValuta { get; set; }
-        public string Atribut { get; set; }
-        public string Interval { get; set; }
+       
 
         void SetProperties()
         {
@@ -54,79 +49,58 @@ namespace KursnaListaGrafikon
             InitializeComponent();
             SetProperties();
 
-            Vreme = "";
-            PocetneValute = new List<string>();
-            KrajnjaValuta = "";
-            Atribut = "";
-            Interval = "";
-
             linijski = new LinijskiGrafikon();
 
             
         }
         private void NacrtajGrafikon(object sender, RoutedEventArgs e)
         {
-            //List<string> pocetne = new List<string>();
-            //foreach (ToggleButton item in panel.Children)
-            //{
-            //    if (item.IsChecked==true)
-            //    {
-            //        pocetne.Add(item.Name);
-            //    }
-            //}
-            //string krajnjaValuta="";
-            //foreach (ToggleButton btn in SingleToggle.Children)
-            //{
-            //    if (btn.IsChecked == true )
-            //    {
-
-            //         krajnjaValuta = (String)btn.Content;
-
-            //}
-
-            //}
-            //string inter = "";
-            //foreach (RadioButton item in interval.Children)
-            //{
-
-            //    if (item.IsChecked == true)
-            //    {
-
-            //         inter = item.Name;
-            //    }
-
-            //}
-            //string period="";
-            //foreach (RadioButton item in vreme.Children)
-            //{
-            //    if (item.IsChecked == true)
-            //    {
-            //         period =(String) item.Content;
-            //    }
-            //}
-            //string atribut = "open";
-            //foreach (string pocetnaValuta in pocetne)
-            //{
-            //    linijski.napraviPar(period, pocetnaValuta, krajnjaValuta, atribut, inter);
-
-            //}
-            //DataContext = this;
-
-
-            /* Ideja za crtanje - Teodora (ne mora ovako, ali bi bilo mnogo lakše)
-            foreach (ToggleButton btn in pocetneValute.Children)
+            List<string> pocetne = new List<string>();
+            foreach (ToggleButton item in pocetneValute.Children)
             {
-                if (btn.IsChecked == true) PocetneValute.Add(btn.Name.ToString());
-            }
-            foreach (string pocetnaValuta in PocetneValute)
-            {
-                if (vreme != "" && krajnjaValuta != "" && atribut != "")
+                if (item.IsChecked == true)
                 {
-                    linijski.NapraviPar(vreme, pocetnaValuta, krajnjaValuta, atribut, interval);
-                    svecasti.NapraviOHLC(vreme, pocetnaValuta, krajnjaValuta, interval);
+                    pocetne.Add(item.Name);
                 }
             }
-            */
+            string krajnjaValuta = "";
+            foreach (ToggleButton btn in krajnjeValute.Children)
+            {
+                if (btn.IsChecked == true)
+                {
+
+                    krajnjaValuta = (String)btn.Content;
+
+                }
+
+            }
+            string inter = "";
+            foreach (RadioButton item in intervali.Children)
+            {
+
+                if (item.IsChecked == true)
+                {
+
+                    inter = item.Name;
+                }
+
+            }
+            string period = "";
+            foreach (RadioButton item in periodi.Children)
+            {
+                if (item.IsChecked == true)
+                {
+                    period = (String)item.Content;
+                }
+            }
+            string atribut = "open";
+            foreach (string pocetnaValuta in pocetne)
+            {
+                linijski.napraviPar(period, pocetnaValuta, krajnjaValuta, atribut, inter);
+
+            }
+            DataContext = this;
+
         }
         private void TableBtn_Click(object sender, RoutedEventArgs e)
         {
@@ -136,13 +110,7 @@ namespace KursnaListaGrafikon
 
         private void Period_Checked(object sender, RoutedEventArgs e)
         {
-            foreach (RadioButton btn in periodi.Children)
-            {
-                if (btn.IsChecked == true)
-                {
-                    Vreme = btn.Content.ToString();
-                }
-            }
+            
 
         }
         private void DisableButtons(object sender, RoutedEventArgs e)
@@ -150,7 +118,7 @@ namespace KursnaListaGrafikon
             foreach (ToggleButton btn in krajnjeValute.Children)
             {
                 if (btn.IsChecked == false) btn.IsEnabled = false;
-                else KrajnjaValuta = btn.Content.ToString();
+                
             }  
         }
 
@@ -159,7 +127,7 @@ namespace KursnaListaGrafikon
             foreach (ToggleButton btn in krajnjeValute.Children)
             {
                 if (btn.IsEnabled == false) btn.IsEnabled = true;
-                else KrajnjaValuta = "";
+                
             }
         }
 
@@ -183,24 +151,17 @@ namespace KursnaListaGrafikon
                 btn.Visibility = Visibility.Hidden;
                 intervalText.Visibility = Visibility.Hidden;
                 if (btn.IsChecked == true) btn.IsChecked = false;
-                Interval = "";
             }
         }
 
         private void Interval_Checked(object sender, RoutedEventArgs e)
         {
-            foreach (RadioButton btn in intervali.Children)
-            {
-                if (btn.IsChecked == true) Interval = btn.Content.ToString();
-            }
+           
         }
         
         private void Attribute_Checked(object sender, RoutedEventArgs e)
         {
-            foreach (RadioButton btn in atributi.Children)
-            {
-                if (btn.IsChecked == true) Atribut = btn.Content.ToString();
-            }
+            
         }
     }
     //API key: M4HJM8TSKJCPJ1WA
